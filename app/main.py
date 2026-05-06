22f90ab8e1cbad3b9cbcbfa880e8bc81a728c54b
Manuel Lombardi <manuellombardi1306@gmail.com>
2026-05-06 17:56:18 -0300

    Seguridad: Eliminada ruta de creación de usuario inicial

--- app/main.py
+++ app/main.py
@@ -7,11 +7,6 @@ from app.models.paciente import Paciente
 from app.models.turnos import Turno
 from app.models.nota import Nota
 from fastapi.staticfiles import StaticFiles
-from sqlalchemy.orm import Session
-from fastapi import Depends
-from app.database import get_db
-from app.auth import hash_password
-from app.models.usuario import Usuario # Asegúrate de que la ruta a tu modelo sea correcta
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -44,21 +39,6 @@ def ver_paciente(request: Request, id: int):
     )
     
 
-
-@app.get("/crear-usuario-inicial")
-def crear_usuario_inicial(db: Session = Depends(get_db)):
-    # Verificamos si ya existe el admin para no duplicarlo
-    existe = db.query(Usuario).filter(Usuario.username == "admin").first()
-    if existe:
-        return {"msg": "El usuario admin ya existe"}
-    
-    nuevo_user = Usuario(
-        username="admin",
-        password=hash_password("1234") # Después la puedes cambiar
-    )
-    db.add(nuevo_user)
-    db.commit()
-    return {"msg": "Usuario admin creado exitosamente en la nube"}
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
